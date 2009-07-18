7b15d6cac19aa9c8d47869ee34e5ba4a858c49d8
BrunoGrasselli <bruno.grasselli@gmail.com>
2009-07-18 16:13:58 -0300

    product

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090718185210) do
+ActiveRecord::Schema.define(:version => 20090718191028) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "makers", :force => true do |t|
     t.string   "name"
@@ -20,5 +27,73 @@ ActiveRecord::Schema.define(:version => 20090718185210) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "phones", :force => true do |t|
+    t.string   "category"
+    t.integer  "ddd"
+    t.integer  "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "picture_comments", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "picture_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pictures", :force => true do |t|
+    t.text     "description"
+    t.string   "image_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
+    t.integer  "album_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", :force => true do |t|
+    t.string   "name"
+    t.integer  "supplier_id"
+    t.integer  "maker_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "suppliers", :force => true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "last_name",                 :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+    t.date     "birthday"
+    t.string   "category"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
